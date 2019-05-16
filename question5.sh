2cea35c75a24b580ba1ed628ac84f6543737a4f6
Fred <fbelange@hotmail.com>
2019-05-16 09:49:58 -0400

    modifié question5.sh

--- question5.sh
+++ question5.sh
@@ -2,7 +2,7 @@
 
 nbrep=1
 while test $nbrep -le 5;do
-    mkdir "repertoire$nbrep"
+    `mkdir "repertoire$nbrep"`
 	echo "Création du sous répertoire \"repertoire\"$nbrep"
-	nbrep=$nbrep+1
+	let nbrep=$nbrep+1
 done
